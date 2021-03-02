3c599e040019d2671deeb0fcf014d9f9b24e7dad
harry0594 <hariatrry123@gmail.com>
2021-03-02 15:49:40 +0530

    init

--- picaltech/pical_app/models.py
+++ picaltech/pical_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class UserDetail(models.Model):
+    name = models.CharField(max_length = 20,blank=True)
+    phone = models.CharField(max_length = 20,blank=True)
+    email = models.EmailField(max_length = 20,blank=True)
+    message = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
--- picaltech/pical_app/views.py
+++ picaltech/pical_app/views.py
@@ -1,8 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from pical_app.models import UserDetail
 
 # Create your views here.
 def index(request):
-    return render(request,'pical_app/index.html')
+    if request.method =='POST':
+        name = request.POST.get("name")
+        phone = request.POST.get("phone")
+        email = request.POST.get("email")
+        message = request.POST.get("message")
+        print(name,phone,email,message)
+        ins = UserDetail(name=name,phone=phone,email=email,message=message )
+        ins.save()
+        print('Contact Details saved to database Successfully!')
+        
+    return render(request,'pical_app/index.html',{})
+
+
     
 def blog(request):
     return render(request,'pical_app/blog')
